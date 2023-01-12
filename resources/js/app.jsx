4eaf09a5cec6aa2e6aab40a616e386f5d6c73e8f
Jude Gumatay <judegumatay007@gmail.com>
2023-01-13 01:03:06 +0800

    changes

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -2,7 +2,7 @@ import React, { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import { HashRouter } from "react-router-dom";
 
-import App from "./pages/App";
+import App from "./Pages/App";
 
 
 
