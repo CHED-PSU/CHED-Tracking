c9be9d292ebb6f51d7487ee9e43bf7b83dfb82ce
CristianJay <chrischanjay20@gmail.com>
2023-03-11 20:48:09 +0800

    sticker

--- resources/js/Pages/Admin/Logs/Forms/Modal/InventorySticker.jsx
+++ resources/js/Pages/Admin/Logs/Forms/Modal/InventorySticker.jsx
@@ -1,8 +1,10 @@
 import axios from "axios";
 import React, { useEffect, useState, useRef } from "react";
+import { useReactToPrint } from "react-to-print";
 import StickerPreview from "./StickerPreview";
 
 export default function InventorySticker(props) {
+    const stickerRef = useRef();
     const modalBody = useRef();
     const [openPreview, setOpenPreview] = useState("close");
 
@@ -11,6 +13,19 @@ export default function InventorySticker(props) {
     }
 
     console.log(openPreview)
+
+    const handlePrint = useReactToPrint({
+        content: () => stickerRef.current,
+        pageStyle: `
+        @media print {
+            @page {
+              size: A4 portrait;
+              margin-top: 0.5in;
+              margin-bottom: 0.5in;
+            }
+          }`,
+        documentTitle: "Inventory Sticker",
+    });
 
     const icsItemsMapper = (items) => {
         return items?.map((data) => {
@@ -54,9 +69,36 @@ export default function InventorySticker(props) {
         <div className={props.className}>
 
             {openPreview === "open" ? (
-                <StickerPreview
-                    className={""}
-                />
+                <div className="fixed inset-0 bg-white w-full h-screen flex flex-col items-center justify-center space-y-10 z-50">
+                    <div className="dark:bg-darkColor-800 h-full w-fit border-x border-[#C8C8C8] pb-10 overflow-y-auto">
+                        <div className="flex justify-between py-5 mb-5 mx-10 border-b-2">
+                            <div className="w-full flex gap-6 py-3 mt-4">
+                                <button
+                                    onClick={() => clickPreview("close")}
+                                    className=""
+                                >
+                                    <i className="fa-solid fa-arrow-left text-2xl text-darkColor-800 dark:text-white"></i>
+                                </button>
+                                <div className="text-left cursor-default">
+                                    <h4 className="text-primary dark:text-white text-2xl font-semibold">
+                                        Inventory Sticker
+                                    </h4>
+                                    <p className="text-sm text-text-gray dark:text-neutral-300">
+                                        This page is hidden. For layout preview only.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="bg-white dark:bg-darkColor-900 rounded-lg border mx-10 p-[0.5in]">
+                            <div ref={stickerRef} className="w-[11.69in] h-[8.27in]">
+                                <div className="text-2xl w-full h-full flex justify-center items-center">Test</div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+
             ) : (
                 ""
             )}
