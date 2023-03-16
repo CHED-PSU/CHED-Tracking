afd8752059c99e7c1909a5b0e6390941a89243da
Edgar <edgartorrequemada57@gmail.com>
2023-03-16 09:27:24 +0800

    api

--- routes/api.php
+++ routes/api.php
@@ -27,116 +27,125 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::post('/login', [AuthController::class, 'login']);
 
 //User Notification
-    //Notification List
-    Route::post('/getNotificationItems',[FormController::class, 'getNotificationItems']);
+//Notification List
+Route::post('/getNotificationItems', [FormController::class, 'getNotificationItems']);
 
-    //Notification Form Item List
-    Route::post('/getNotifSecListItems',[ItemController::class,'getNotifSecListItems']);
+//Notification Form Item List
+Route::post('/getNotifSecListItems', [ItemController::class, 'getNotifSecListItems']);
 
-    //Notification Form Details
-    Route::post('/getFormDetails',[FormController::class,'getFormDetails']);
+//Notification Form Details
+Route::post('/getFormDetails', [FormController::class, 'getFormDetails']);
 
-    //Accept and Decline Issued form from notification
-    Route::post('acceptIssuedForm',[FormController::class,'acceptIssuedForm']);
-    Route::post('declineIssuedForm',[FormController::class,'declineIssuedForm']);
+//Accept and Decline Issued form from notification
+Route::post('acceptIssuedForm', [FormController::class, 'acceptIssuedForm']);
+Route::post('declineIssuedForm', [FormController::class, 'declineIssuedForm']);
 
 //User Home Area
-    //recent Issuance
-        Route::post('HomeData',[FormController::class,'HomeData']);
+//recent Issuance
+Route::post('HomeData', [FormController::class, 'HomeData']);
 
 //User Items Area
-    //Items fetcher
-        Route::post('getuserIndividualItems',[ItemController::class,'getuserIndividualItems']);
-    //Item Request Form Fetcher
-        Route::post('getItemRequestData',[ItemController::class, 'getItemRequestData']);
 
-    //return items to admin
-        Route::post('returnItemsToAdmin',[ItemController::class, 'returnItemsToAdmin']);
+//Items fetcher
+Route::post('getuserIndividualItems', [ItemController::class, 'getuserIndividualItems']);
+//Item Request Form Fetcher
+Route::post('getItemRequestData', [ItemController::class, 'getItemRequestData']);
+
+//return items to admin
+Route::post('returnItemsToAdmin', [ItemController::class, 'returnItemsToAdmin']);
 
 //User Logs Area
-    //get data for tables
-        //ICS
-            Route::post('getICS',[FormController::class,'getICS']);
 
-            //ICS details
-                Route::post('getIcsDetails', [FormController::class, 'getIcsDetails']);
-        //PAR
-            Route::post('getPAR',[FormController::class,'getPAR']);
+//get data for tables
+//ICS
+Route::post('getICS', [FormController::class, 'getICS']);
 
-            //PAR details
-                Route::post('getParDetails', [FormController::class, 'getParDetails']);
-        //Individual Items
-            Route::post('getIndividualItems',[FormController::class,'getIndividualItems']);
+//ICS details
+Route::post('getIcsDetails', [FormController::class, 'getIcsDetails']);
+//PAR
+Route::post('getPAR', [FormController::class, 'getPAR']);
+
+//PAR details
+Route::post('getParDetails', [FormController::class, 'getParDetails']);
+//Individual Items
+Route::post('getIndividualItems', [FormController::class, 'getIndividualItems']);
 
 //User Requests Area
-    //Pending Requests
-        //Pending and Accepted Count
-            Route::get('getPendingAcceptedRequests', [GeneralController::class, 'getPendingAcceptedRequests']);
 
-        //Pending Data
-            Route::post('getPendingRequests', [FormController::class, 'getPendingRequests']);
+//Pending Requests
+//Pending and Accepted Count
+Route::get('getPendingAcceptedRequests', [GeneralController::class, 'getPendingAcceptedRequests']);
 
+//Pending Data
+Route::post('getPendingRequests', [FormController::class, 'getPendingRequests']);
 
 //Admin Notification
-    //get admin notification items
-        Route::get('getAdminNotification',[FormController::class, 'getAdminNotification']);
-        Route::get('getAdminRequest',[FormController::class, 'getAdminRequest']);
 
+//get admin notification items
+Route::get('getAdminNotification', [FormController::class, 'getAdminNotification']);
+Route::get('getAdminRequest', [FormController::class, 'getAdminRequest']);
 //Admin Dashboard
-    Route::get('getAdminDashboardData',[GeneralController::class, 'getAdminDashboardData']);
-
+Route::get('getAdminDashboardData', [GeneralController::class, 'getAdminDashboardData']);
 //Admin Pending Items
-    Route::get('getPendingItems',[ItemController::class, 'getPendingitems']);
-        //get Returned Items Data
-            Route::post('getReturnedItemsData',[FormController::class, 'getReturnedItemsData']);
-                //accept pending request
-                    Route::post('acceptPendingRequest',[FormController::class, 'acceptPendingRequest']);
-                //decline pending request
-                    Route::post('declinePendingRequest',[FormController::class, 'declinePendingRequest']);
-//Admin Logs Area
-    // Admin Users Data Fetcher
-        Route::get('getUserLists',[GeneralController::class, 'getUserLists']);
-            //get User Ics Controls
-                Route::post('getUserIcsControls',[FormController::class, 'getUserIcsControls']);
-            //get User PAR Controls
-                Route::post('getUserParControls',[FormController::class, 'getUserParControls']);
-//Admin Returned Items
-    // Admin Returned Items Fetcher
-        Route::get('getReturnedItems',[ItemController::class,'getReturnedItems']);
-            //Returned Items Data Fetcher
-                Route::post('getAdminReturnedItemsData',[ItemController::class,'getAdminReturnedItemsData']);
-            //Admin return pre save
-                Route::post('returnItemsPreSave',[ItemController::class, 'returnItemsPreSave']);
-            //Admin return Items post save
-            Route::post('returnItemsPostSave',[ItemController::class, 'returnItemsPostSave']);
-            //admin return items change status
-            Route::post('returnedItemsChangeStatus',[ItemController::class, 'returnedItemsChangeStatus']);
-            //admin User Fetcher
-            Route::get('getUsers',[GeneralController::class, 'getUsers']);
-                //admin move item to inventories
-                    Route::post('moveToInventories',[ItemController::class, 'moveToInventories']);
+Route::get('getPendingItems', [ItemController::class, 'getPendingitems']);
+//get Returned Items Data
+Route::post('getReturnedItemsData', [FormController::class, 'getReturnedItemsData']);
+//accept pending request
+Route::post('acceptPendingRequest', [FormController::class, 'acceptPendingRequest']);
+//decline pending request
+Route::post('declinePendingRequest', [FormController::class, 'declinePendingRequest']);
 
-                //Admin move item to unserviceable item
-                    Route::post('moveItemstoUnserviceableItems',[ItemController::class, 'moveItemstoUnserviceableItems']);
+//Admin Logs Area
+
+// Admin Users Data Fetcher
+Route::get('getUserLists', [GeneralController::class, 'getUserLists']);
+Route::get('getUsersById', [GeneralController::class, 'getUsersById']);
+//get User Ics Controls
+Route::post('getUserIcsControls', [FormController::class, 'getUserIcsControls']);
+//get User PAR Controls
+Route::post('getUserParControls', [FormController::class, 'getUserParControls']);
+
+//Admin Returned Items
+
+// Admin Returned Items Fetcher
+Route::get('getReturnedItems', [ItemController::class, 'getReturnedItems']);
+//Returned Items Data Fetcher
+Route::post('getAdminReturnedItemsData', [ItemController::class, 'getAdminReturnedItemsData']);
+//Admin return pre save
+Route::post('returnItemsPreSave', [ItemController::class, 'returnItemsPreSave']);
+//Admin return Items post save
+Route::post('returnItemsPostSave', [ItemController::class, 'returnItemsPostSave']);
+//admin return items change status
+Route::post('returnedItemsChangeStatus', [ItemController::class, 'returnedItemsChangeStatus']);
+//admin User Fetcher
+Route::get('getUsers', [GeneralController::class, 'getUsers']);
+//admin move item to inventories
+Route::post('moveToInventories', [ItemController::class, 'moveToInventories']);
+
+//Admin move item to unserviceable item
+Route::post('moveItemstoUnserviceableItems', [ItemController::class, 'moveItemstoUnserviceableItems']);
 
 //Admin Inventories
-    //Inventory Items Fetcher
-    Route::get('getItemsofInventories',[ItemController::class,'getItemsofInventories']);
-    Route::post('getInventorySorted',[ItemController::class, 'getInventorySorted']);
-    //admin return item to previous owwner
-    Route::post('returnToPreviousOwner',[ItemController::class, 'returnToPreviousOwner']);
-    //admin issue to another owner
-        Route::post('assignToAnotherUser',[ItemController::class, 'assignToAnotherUser']);
 
-        //Sorted Multi Return to prev owner
-        Route::post('multiReturnToPrevOwner',[ItemController::class, 'multiReturnToPrevOwner']);
-        //Sorted Multi Return or Renew
-        Route::post('multiReturnAndRenew',[ItemController::class, 'multiReturnAndRenew']);
+//Inventory Items Fetcher
+Route::get('getItemsofInventories', [ItemController::class, 'getItemsofInventories']);
+Route::post('getInventorySorted', [ItemController::class, 'getInventorySorted']);
+//admin return item to previous owwner
+Route::post('returnToPreviousOwner', [ItemController::class, 'returnToPreviousOwner']);
+//admin issue to another owner
+Route::post('assignToAnotherUser', [ItemController::class, 'assignToAnotherUser']);
+
+//Sorted Multi Return to prev owner
+Route::post('multiReturnToPrevOwner', [ItemController::class, 'multiReturnToPrevOwner']);
+//Sorted Multi Return or Renew
+Route::post('multiReturnAndRenew', [ItemController::class, 'multiReturnAndRenew']);
+
 //Admin Unserviceable Items
-    //Admin Unserviceable Items
-        Route::get('getUnserviceableItems',[ItemController::class, 'getUnserviceableItems']);
+
+//Admin Unserviceable Items
+Route::get('getUnserviceableItems', [ItemController::class, 'getUnserviceableItems']);
 
 //Admin forecasting Items
-    Route::get('forecast',[ForecastingController::class, 'forecast']);
-    Route::post('forecastSpecific',[ForecastingController::class, 'forecastSpecific']);
-    Route::get('totalCostPerYear',[ForecastingController::class,'totalCostPerYear']);
+Route::get('forecast', [ForecastingController::class, 'forecast']);
+Route::post('forecastSpecific', [ForecastingController::class, 'forecastSpecific']);
+Route::get('totalCostPerYear', [ForecastingController::class, 'totalCostPerYear']);
